e54be8d2799ed2e99989857030243f2114606fda
Onur <onurpulat.op@gmail.com>
2018-12-19 23:45:16 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,11 +1,10 @@
 <?php
 try{
-
 	$db=new PDO("mysql:host=127.0.0.1;dbname=kayitbilgisi; charset:utf-8",'root','');
 }
 catch ( PDOException $e )
 {
-     print $e->getMessage();
+	print $e->getMessage();
 }
 
 ?>
